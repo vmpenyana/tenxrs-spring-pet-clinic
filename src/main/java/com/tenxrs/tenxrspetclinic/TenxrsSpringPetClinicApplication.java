490d36a8c2a03ab6715c3fa358098943a1c60953
vmpenyana <vincent@tenxrs.com>
2023-08-21 08:14:31 +0200

    Initialization of the project

---
+++ src/main/java/com/tenxrs/tenxrspetclinic/TenxrsSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.tenxrs.tenxrspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenxrsSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenxrsSpringPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tenxrs/tenxrspetclinic/TenxrsSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenxrs.tenxrspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenxrsSpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
