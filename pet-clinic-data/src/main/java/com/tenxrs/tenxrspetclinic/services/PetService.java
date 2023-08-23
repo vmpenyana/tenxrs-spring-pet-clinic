497ccbda85c426bd895942f6d810ef28dfa4630b
vmpenyana <vincent@tenxrs.com>
2023-08-23 12:43:16 +0200

    Refactored interfaces with a common interface.

--- pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/services/PetService.java
@@ -5,11 +5,4 @@ import com.tenxrs.tenxrspetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
-}
+public interface PetService extends CrudService<Pet, Long> {}
--- pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/services/VetService.java
@@ -3,15 +3,4 @@ package com.tenxrs.tenxrspetclinic.services;
 
 import com.tenxrs.tenxrspetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findByLastName();
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
-}
+public interface VetService extends CrudService<Vet, Long> { }
