450e9de098e93819fc0932fdd44a8c849fcd0f02
vmpenyana <vincent@tenxrs.com>
2023-08-23 08:03:43 +0200

    Added a BaseEntity and extend it to relevant model.

--- pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.tenxrs.tenxrspetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.tenxrs.tenxrspetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
