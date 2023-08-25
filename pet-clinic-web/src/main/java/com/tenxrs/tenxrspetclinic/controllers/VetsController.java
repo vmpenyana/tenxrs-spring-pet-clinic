36006fd7250d673c4df344c4e090f98a6db7b52a
vmpenyana <vincent@tenxrs.com>
2023-08-25 07:55:06 +0200

    Listing vets from bootstrap data.

--- pet-clinic-web/src/main/java/com/tenxrs/tenxrspetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/tenxrs/tenxrspetclinic/controllers/VetsController.java
@@ -1,13 +1,23 @@
 package com.tenxrs.tenxrspetclinic.controllers;
 
+import com.tenxrs.tenxrspetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("/vets")
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
 
