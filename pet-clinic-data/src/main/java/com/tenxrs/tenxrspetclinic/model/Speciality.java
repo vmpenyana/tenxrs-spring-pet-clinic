86d4a74c89186e3e750abeec4ccbde04516549bb
vmpenyana <vincent@tenxrs.com>
2023-08-27 14:56:49 +0200

    Added a Speciality entity and linked it to Vet.

---
+++ pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.tenxrs.tenxrspetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
