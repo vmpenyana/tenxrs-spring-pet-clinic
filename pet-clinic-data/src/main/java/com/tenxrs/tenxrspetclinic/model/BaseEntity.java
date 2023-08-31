8ce7a4499980ab390f5926a9a8ed8f57dba8e00e
vmpenyana <vincent@tenxrs.com>
2023-08-31 08:03:20 +0200

    Implemented JPA base entity.

--- pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/tenxrs/tenxrspetclinic/model/BaseEntity.java
@@ -1,9 +1,21 @@
 package com.tenxrs.tenxrspetclinic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import java.io.Serializable;
 
+@MappedSuperclass
+/**
+ * Annotation above indicates that the class below will be/is a base JPA entity.
+ *  "We don't need this specific class mapped to the database?"
+ */
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
